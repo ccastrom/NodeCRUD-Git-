63e04a39f42eb7b3e6f8612b68c1b5055024d637
BRAVOSIX <claudiocastrom7@gmail.com>
2023-12-21 19:55:54 -0300

    Modification of order in 'app.use()' in 'app.js' due to the occurrence of an error.

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
-app.use('/api/vehicles',routeVehicles);
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+app.use('/api/vehicles',routeVehicles);
+
 
 
 
